cbaad15e7c6f47c8badd99381b24cbca4a64a3b5
Willian <willian.abdom@hotmail.com>
2019-04-17 14:24:17 -0300

    adding store, update and list to service model

---
+++ src/modules/services/router.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import ServiceController from './controller';
+const serviceRouter = express.Router();
+
+serviceRouter.patch('/update/:id', ServiceController.update);
+serviceRouter.get('/:id', ServiceController.show);
+serviceRouter.get('/', ServiceController.showAll);
+serviceRouter.post('/register', ServiceController.store);
+
+export default serviceRouter;
