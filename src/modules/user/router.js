06f3e1fbad19f25b06eca5524293759250ba7b97
Willian <willian.abdom@hotmail.com>
2019-04-20 00:29:57 -0300

    adding auth middlware

--- src/modules/user/router.js
+++ src/modules/user/router.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import UserController from './controller';
+import auth from '../middlewares/auth';
 const userRouter = express.Router();
 
 userRouter.post('/register', UserController.store);
 userRouter.post('/login', UserController.login);
-userRouter.patch('/update/:id', UserController.update);
+userRouter.patch('/update/', auth, UserController.update);
 userRouter.get('/:id', UserController.show);
 userRouter.get('/', UserController.showAll);
 userRouter.delete('/:id', UserController.delete);
