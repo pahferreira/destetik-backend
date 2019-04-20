9893b499aafcaf34667035d16ee69aaa1e4395a4
Pah Ferreira <pah.dsf@gmail.com>
2019-04-20 11:40:07 -0300

    Fixing Auth Middleware

--- src/modules/middlewares/auth.js
+++ src/modules/middlewares/auth.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-    const token_header = req.headers.auth;
-    if (!token_header) return res.status(401).send({error: "Token was not send."});
+  const token_header = req.header('Authorization').split('Bearer ')[1];
+  console.log(token_header);
+  if (!token_header)
+    return res.status(401).send({ error: 'Token was not send.' });
 
-    jwt.verify(token_header, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(401).send({error: "Token is invalid."});
-        res.locals.auth_data = decoded;
-        return next();
-    })
-}
-
+  jwt.verify(token_header, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).send({ error: 'Token is invalid.' });
+    res.locals.auth_data = decoded;
+    return next();
+  });
+};
 
 export default auth;
