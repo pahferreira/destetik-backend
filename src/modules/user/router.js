586c9343c94e989723cee97650d5805bce79765b
Willian <willian.abdom@hotmail.com>
2019-04-22 18:19:59 -0300

    update delete controller to send ID on header

--- src/modules/user/router.js
+++ src/modules/user/router.js
@@ -5,9 +5,9 @@ const userRouter = express.Router();
 
 userRouter.post('/register', UserController.store);
 userRouter.post('/login', UserController.login);
-userRouter.patch('/update/', auth, UserController.update);
+userRouter.patch('/update', auth, UserController.update);
 userRouter.get('/:id', UserController.show);
 userRouter.get('/', UserController.showAll);
-userRouter.delete('/:id', UserController.delete);
+userRouter.delete('/delete', UserController.delete);
 
 export default userRouter;
