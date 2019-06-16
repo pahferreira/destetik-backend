28987213f61c3272a46a504c0b1c7b95b89d794b
Willian <willian.abdom@gmail.com>
2019-06-16 20:38:05 -0300

    performedService populate fix

--- src/modules/performedService/controller.js
+++ src/modules/performedService/controller.js
@@ -19,9 +19,17 @@ class PerformedServiceController {
 
   async showClient(req, res) {
     try {
-      const services = await PerformedService.find({ clientId: req.user.id }).populate(
-        'clientId'
-      ).populate('providedServiceId').populate({path:'providedServiceId', populate: {path:'userId'}});
+      const services = await PerformedService.find({ clientId: req.user.id })
+      .populate('clientId', '_id name')
+      .populate('providedServiceId')
+      .populate({path:'providedServiceId', populate: {
+        path:'userId',
+        select: {'_id':1, 'name':1}
+      }})
+      .populate({path:'providedServiceId', populate: {
+        path:'serviceId',
+        select: {'_id':1, 'name':1}
+      }});
       return res.json(services);
     } catch (err) {
       console.log(err);
@@ -31,9 +39,17 @@ class PerformedServiceController {
   async showProvider(req, res) {
     try {
       const providedServices = await ProvidedService.find({ userId : req.user.id }).select('id');
-      const services = await PerformedService.find({ providedServiceId: { $in : providedServices} }).populate(
-        'clientId'
-      ).populate('providedServiceId').populate({path:'providedServiceId', populate: {path:'userId'}});
+      const services = await PerformedService.find({ providedServiceId: { $in : providedServices} })
+      .populate('clientId', '_id name')
+      .populate('providedServiceId')
+      .populate({path:'providedServiceId', populate: {
+        path:'userId',
+        select: {'_id':1, 'name':1}
+      }})
+      .populate({path:'providedServiceId', populate: {
+        path:'serviceId',
+        select: {'_id':1, 'name':1}
+      }});
       return res.json(services);
     } catch (err) {
       console.log(err);
